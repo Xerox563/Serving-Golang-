f1d7f1154e14a19c8fcb825a7dafe3b368ddd8fb
Amit Gangwar <gangwaramit449@gmail.com>
2025-09-19 00:02:42 +0530

    [addon] Updated all the types [basic onms]

---
+++ 02Variables/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+// jwttoken := xerojfjwfwwwnv  // error[walrus operator cant be used outside any method: will through the error.]
+var y = "xerojfjwfwwwnv" // fine
+var x int = 34  // fine
+const LoginToken string = "yrruiojqfvbsndvlk3y3428nbkjnnn42klt"  // L -> public keyword -> accessible global
+func main() {
+    
+	//string
+	var username string ="Ritika Mishra"
+	fmt.Println(username)
+    fmt.Printf("Variable is of type: %T \n",username)
+    
+	//bool
+	var isLogged bool = false
+	fmt.Println(isLogged)
+	fmt.Printf("Variable is of type: %T \n",isLogged)
+
+	//uint/int
+	var val uint8 = 255  // Use when specifiic to OS.
+	var x int = 256 // Use safely for all .
+	fmt.Println(val,x)
+	fmt.Printf("Variable is of type: %T \n",val)
+
+	//float
+	var val1 float32 = 255.4327498374710  // stores till 5 decimal placess
+	var val2 float64 = 746.7413324234233641379 // stores till >5 decimal placess
+	fmt.Println(val1,val2)
+	
+	// default values and some aliases
+	var anotherVariable bool  // 0 -> empty -> false
+	fmt.Println(anotherVariable)
+	fmt.Printf("Variable is of type:%T \n",anotherVariable)
+    
+	// implicit type -> decided by lexer [automatically decides by lexer some type]
+	var web = "alphaxerox.com"
+	fmt.Println(web)
+	fmt.Printf("Variable is of type:%T \n",web)
+	
+	// no var style
+    numberofuser := 3000.1
+	fmt.Println(numberofuser)
+	
+	
+	// global variable
+	println(LoginToken)
+	fmt.Printf("Variable is of type:%T \n",LoginToken)
+}
