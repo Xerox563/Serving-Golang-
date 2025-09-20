2177a4b47d1f506ea9bde4e5467d09afe3fd7f15
Amit Gangwar <gangwaramit449@gmail.com>
2025-09-20 11:41:43 +0530

    [Addon] Maps ,looping on map

---
+++ 08Maps/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Lets finish the Maps !!")
+
+    lang := make(map[string]string)
+    lang["js"] = "Javascript"
+    lang["py"] = "Python"
+    lang["rb"] = "Ruby"
+    lang["ts"] = "Typescript"
+    lang["go"] = "Golang"
+
+    fmt.Println("List of All Languages:", lang)
+    fmt.Println("Language with js:", lang["js"])
+
+    delete(lang, "rb")
+    fmt.Println("List of All Languages after deleting rb:", lang)
+
+    // loops
+    for key, value := range lang {
+        fmt.Printf("for Key %v, value is %v\n", key, value)
+    }
+
+    for _, value := range lang {
+        fmt.Printf("value is %v\n", value)
+    }
+}
+
+
+// fmt.Println -> just prints the value
+// fmt.Printf -> %v [for filling values],%T, all used here
