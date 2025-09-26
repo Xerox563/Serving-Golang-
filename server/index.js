604a9956a7d96b37510118398b61560662fe55c3
xerox563 <amit0101ag@gmail.com>
2025-09-26 09:30:32 +0530

    [Addon] How to create Nodejs Server [inorder to fetch the data and test ..]

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to LCU  server");
+});
+
+app.get("/get", (req, res) => {
+  res.status(200).json({ message: "Hello from gangwaramit.in" });
+});
+
+app.post("/post", (req, res) => {
+  let myJson = req.body; // our JSON
+
+  res.status(200).send(myJson);
+});
+
+app.post("/postform", (req, res) => {
+  res.status(200).send(JSON.stringify(req.body));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
