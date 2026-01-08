df51cde46303534a24cc0b4e4261da016d385b57
xerox563 <amit0101ag@gmail.com>
2026-01-08 22:28:24 +0530

    Initial empty branchh

--- Practice-Rect/src/Components/Header.jsx
+++
@@ -1,5 +0,0 @@
-const Header = () => {
-  return <h1> This is the Header </h1>;
-};
-
-export default Header;
--- mathFun/index.js
+++
@@ -1,5 +0,0 @@
-const { countConsonants } = require("./countConstants");
-const { countVowels } = require("./countVowels");
-const { isPalindrome } = require("./isPalindrome");
-
-module.exports = { countConsonants, countVowels, isPalindrome };
