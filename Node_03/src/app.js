327d9f741f787d0dd1b9648039a8aeeb16da347f
xerox563 <amit0101ag@gmail.com>
2026-01-10 11:00:50 +0530

    [Add] added code for feed api and getUserByEmail

--- Node_03/src/app.js
+++ Node_03/src/app.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "../.env" });
+//
 const express = require("express");
 const connectDB = require("./config/database");
 const User = require("./models/user");
@@ -6,18 +6,54 @@ const app = express();
 const PORT = process.env.PORT || 4444;
 // console.log("MONGO_URI:", process.env.MONGO_URI);
 
+app.use(express.json());
+// It reads the json object converts into the js object and adds them to the request object : req: now req.body -> js object
+
 // Post some data into the database
 app.post("/signup", async (req, res) => {
   // creating the new instance of user model
-  const user = new User({
-    firstName: "Virat",
-    lastName: "Kohli",
-    emailID: "xerox12@gamil.com",
-    passwprd: "xerox1@54321",
-  });
+  console.log(req.body);
+  const user = new User(req.body);
+  // dummy data
+  //   const user = new User({
+  //     firstName: "Virat",
+  //     lastName: "Kohli",
+  //     emailID: "xerox12@gamil.com",
+  //     passwprd: "xerox1@54321",
+  //   });
   try {
     await user.save(); // returns a promise
     res.send("User Added Successfully !!");
+  } catch (err) {
+    res.status(400).send("Error Saving the user: ", err.message);
+  }
+});
+
+app.get("getUserByEmail", async (req, res) => {
+  const userEmailId = req.body.emailId;
+  try {
+    console.log(userEmailId);
+    const user = await User.find({ firstName: userEmailId });
+    if (!user) {
+      res.status(400).send("User not Found !!");
+    } else {
+      res.send(user);
+      console.log(user);
+    }
+  } catch (err) {
+    res.status(400).send("Error Saving the user: ", err.message);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const user = await User.find();
+    if (!user) {
+      res.status(404).send("User not found !!");
+    } else {
+      res.send(user);
+      console.log(user);
+    }
   } catch (err) {
     res.status(400).send("Error Saving the user: ", err.message);
   }
@@ -28,7 +64,7 @@ connectDB()
     console.log("✅ Database Connected Successfully !!");
     // Since DB connection is successful so listen to the user request by server .
     app.listen(PORT, () => {
-      console.log("Server running on Port: 8888");
+      console.log(`Server running on Port: ${PORT}`);
     });
   })
   .catch((err) => {
